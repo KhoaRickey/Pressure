6d02f725057bdfeda00b3e760a634859b1d9f814
Khoa <beckhoa93@gmail.com>
2015-05-22 14:58:27 +0300

    move compare value to the top, backpress, test device sensor, rotate etc...

--- app/src/main/java/com/logovisa/pressure/MainActivity.java
+++ app/src/main/java/com/logovisa/pressure/MainActivity.java
@@ -1,37 +1,38 @@
 package com.logovisa.pressure;
 
+
+import android.app.AlertDialog;
 import android.app.Service;
+import android.content.DialogInterface;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
-import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 
 public class MainActivity extends ActionBarActivity implements SensorEventListener{
-    public String CurrentPressure;
+    public float CurrentPressure;
+    public float CurrentCompare = 0;
+    public int id = 0;
+    public List idList = new ArrayList();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
         //Get the reference to the sensor manager
         SensorManager sensorManager = (SensorManager) getSystemService(Service.SENSOR_SERVICE);
 
@@ -52,7 +53,18 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         ssData.setText(data);**/
         // Look for barometer sensor
         Sensor pS = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
-        sensorManager.registerListener((SensorEventListener) this, pS,SensorManager.SENSOR_DELAY_NORMAL);
+        if(pS == null){
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setMessage("Your device doesn't have barometer sensor");
+            alert.setCancelable(false);
+            alert.setPositiveButton("Exit",new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    System.exit(0);
+                }
+            });
+            alert.create().show();
+        }
+        sensorManager.registerListener((SensorEventListener) this, pS, sensorManager.SENSOR_DELAY_NORMAL );
 
         //Add button handling
         Button add = (Button)findViewById(R.id.add);
@@ -62,13 +74,73 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
                 EditText input = (EditText)findViewById(R.id.editText);
                 //StringBuilder userInput = new StringBuilder();
                 //userInput.append(input.getText() + ": " + CurrentPressure + "\n");
-                TextView userInputView = (TextView)findViewById(R.id.userInput);
-                userInputView.append(input.getText() + ": " + CurrentPressure + " hPa" + "\n");
-                TextView resultText1 = (TextView)findViewById(R.id.resultText1);
-                resultText1.setVisibility(View.VISIBLE);
+                //TextView userInputView = (TextView)findViewById(R.id.userInput);
+                //userInputView.append(input.getText() + ": " + CurrentPressure + " hPa" + "\n");
+                TableLayout table = (TableLayout)findViewById(R.id.tableData);
+                TableRow row = new TableRow(getApplicationContext());
+
+                //update room name from input field
+                TextView inputRoom = new TextView(getApplicationContext());
+                inputRoom.setText(input.getText());
+                row.addView(inputRoom);
+
+                //add current pressure of the room above
+                TextView currentPressure = new TextView(getApplicationContext());
+                currentPressure.setGravity(Gravity.CENTER_HORIZONTAL);
+                currentPressure.setId(id + 100);
+                String cP = Float.toString(CurrentPressure);
+                currentPressure.setText(cP);
+                row.addView(currentPressure);
+
+                //compare Compare value with the room's value
+                TextView comparePressure = new TextView(getApplicationContext());
+                idList.add(id);
+                comparePressure.setId(id);
+                float diff;
+                if(CurrentCompare == 0) diff = 0;
+                else diff = CurrentCompare - CurrentPressure;
+                String diffString = Float.toString(diff);
+                comparePressure.setText(diffString);
+                row.addView(comparePressure);
+
+                table.addView(row);
+                input.setText("");
+                id++;
+            }
+        });
+
+        //Compare button handling
+        final Button compare = (Button)findViewById(R.id.compare);
+        compare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = (EditText)findViewById(R.id.editText);
+                TextView compareValue = (TextView)findViewById(R.id.compareValue);
+                CurrentCompare = CurrentPressure;
+
+                //If there is a string in input field, it will be updated next to the Compare Value. If there is none, it just update the compare value
+                String inputTxt = input.getText().toString();
+                String compare = compareValue.getText().toString();
+                String[] compareS = compare.split(":");
+                if(inputTxt.matches("")) compareValue.setText(compareS[0] + ": " + CurrentCompare);
+                else compareValue.setText("Compare Value (" + input.getText() + ") : " + CurrentCompare);
+                updateCompare();
                 input.setText("");
             }
         });
+    }
+
+    //Comparing functon and update the values
+    void updateCompare(){
+        for(int i = 0; i < idList.size(); i++){
+            TextView comparePressure = (TextView)findViewById(idList.indexOf(i));
+            TextView currentPressure = (TextView)findViewById(idList.indexOf(i) + 100);
+            String current = currentPressure.getText().toString();
+            float currentP = Float.parseFloat(current);
+            float newCompare = CurrentPressure - currentP;
+            String newCompareTxt = Float.toString(newCompare);
+            comparePressure.setText(newCompareTxt);
+        }
     }
 
     @Override
@@ -79,12 +151,17 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
     public void onSensorChanged(SensorEvent event) {
         float[] values = event.values;
         //Convert float value to string with desired format
-        DecimalFormat df = new DecimalFormat("#.####");
-        String dx=df.format(values[0]);
-        CurrentPressure = dx;
+        //DecimalFormat df = new DecimalFormat("#.####");
+        //String dx=df.format(values[0]);
+        CurrentPressure = values[0];
         TextView result = (TextView)findViewById(R.id.resultText);
         //Show current pressure value to user
-        result.setText("Current atmospheric pressure:   " + dx + " hPa");
+        result.setText("Current atmospheric pressure:   " + values[0] + " hPa");
+    }
+
+    @Override
+    public void onBackPressed() {
+        createExitMsgBox();
     }
 
     @Override
@@ -107,5 +184,28 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void createExitMsgBox()
+    {
+        AlertDialog.Builder alertDlg = new AlertDialog.Builder(this);
+        alertDlg.setMessage("Oletko varma että haluat sulkea sovelluksen?");
+        alertDlg.setCancelable(false); // We avoid that the dialong can be cancelled, forcing the user to choose one of the options
+        alertDlg.setPositiveButton("Kyllä", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        System.exit(0);
+                    }
+                }
+        );
+
+        alertDlg.setNegativeButton("Ei", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // We do nothing
+
+            }
+
+        });
+        alertDlg.create().show();
     }
 }
